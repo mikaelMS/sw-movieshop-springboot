6a0cdeafa4464fe9454b69ce5c9f7e101413d9ff
mikaelMS <schmidt.m.work@googlemail.com>
2020-03-25 16:52:52 +0100

    initilized project

---
+++ src/main/java/com/oth/sw/mikesmovieshop/mikesmovieshop/MikesmovieshopApplication.java
@@ -0,0 +1,13 @@
+package com.oth.sw.mikesmovieshop.mikesmovieshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikesmovieshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikesmovieshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oth/sw/mikesmovieshop/mikesmovieshop/MikesmovieshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oth.sw.mikesmovieshop.mikesmovieshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikesmovieshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
