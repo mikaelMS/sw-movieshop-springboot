3f3979f1d77359f3853a42a2405bc44ec6e64d26
mikaelMS <schmidt.m.work@googlemail.com>
2020-03-26 18:58:14 +0100

    added index and first fragment

---
+++ src/main/java/com/oth/sw/mikesmovieshop/mikesmovieshop/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.oth.sw.mikesmovieshop.mikesmovieshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+//    @Autowired
+//    private KundeServiceIF kundeService;
+
+    @RequestMapping("/")
+    public String start() {
+        return "index";
+    }
+
+//    @RequestMapping("/registrieren")
+//    public String registrieren(
+//            @ModelAttribute("vorname") String vname,
+//            @ModelAttribute("nachname") String nname,
+//            Model model
+//    ) {
+//        Kunde kunde = new Kunde();
+//        kunde.setVorname(vname);
+//        kunde.setNachname(nname);
+//        kunde = kundeService.kundeAnlegen(kunde);
+//        model.addAttribute("kundennr", kunde.getKundenNr());
+//
+//        return "kundenkonto";
+//    }
+
+}
