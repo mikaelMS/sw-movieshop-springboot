42c0aa3c79361caf05214fb5f25905b5439c1398
mikaelMS <schmidt.m.work@googlemail.com>
2020-03-30 17:53:22 +0200

    updated login but still bugged

--- src/main/java/com/oth/sw/mikesmovieshop/mikesmovieshop/controller/HomeController.java
+++ src/main/java/com/oth/sw/mikesmovieshop/mikesmovieshop/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.oth.sw.mikesmovieshop.mikesmovieshop.controller;
 
-import com.oth.sw.mikesmovieshop.mikesmovieshop.entity.Movie;
-import com.oth.sw.mikesmovieshop.mikesmovieshop.interfaces.MovieServiceIF;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,27 +8,11 @@ public class HomeController {
 
     @RequestMapping("/")
     public String start() {
-        return "index.html";
+        return "index";
     }
 
     @RequestMapping("/cart")
     public String viewCart() {
         return "sites/cart";
     }
-
-//    @RequestMapping("/registrieren")
-//    public String registrieren(
-//            @ModelAttribute("vorname") String vname,
-//            @ModelAttribute("nachname") String nname,
-//            Model model
-//    ) {
-//        Kunde kunde = new Kunde();
-//        kunde.setVorname(vname);
-//        kunde.setNachname(nname);
-//        kunde = kundeService.kundeAnlegen(kunde);
-//        model.addAttribute("kundennr", kunde.getKundenNr());
-//
-//        return "kundenkonto";
-//    }
-
 }
